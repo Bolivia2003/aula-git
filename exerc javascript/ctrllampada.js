2514bf32c33bc41a82f4a3c0578cf70393055e45
Bolivia2003 <lucasroalvarez12@gmail.com>
2022-06-30 21:33:25 -0300

    uma menssagem

---
+++ exerc javascript/ctrllampada.js
@@ -0,0 +1,10 @@
+
+		function abreporta(){
+			document.getElementById("porta"). src="aberto.jpg";
+		}
+		function fechaporta(){
+			document.getElementById("porta"). src="fechado.jpg";
+		}
+		function desbloqueado(){
+			document.getElementById("porta"). src="desbloqueado.jpg";
+		}
